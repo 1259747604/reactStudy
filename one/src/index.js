321375da83b4915d3e184efc6e5432dfff8791d4
tt <1259747604@qq.com>
2019-12-08 17:46:28 +0800

    提交

--- one/src/index.js
+++ one/src/index.js
@@ -7,7 +7,9 @@ import ReactDom from 'react-dom';
 // import Composition from './Composition'
 // import Hoc from './HOC/HOC';
 // import Context1 from './HOC/Context1'
-import Context2 from './HOC/Context2';
+// import Context2 from './HOC/Context2';
+import FirstRedux from './reduxFloder/FirstRedux';
+import store from './reduxFloder/store';
 
 // ReactDom.render(<App/>,document.getElementById('root'))
 //演示生命周期
@@ -21,4 +23,34 @@ import Context2 from './HOC/Context2';
 //高阶组件
 // ReactDom.render(<Hoc title="一个属性"/>,document.getElementById('root'))
 //组件上下文
-ReactDom.render(<Context2 />, document.getElementById('root'));
+// ReactDom.render(<Context2 />, document.getElementById('root'));
+// 原生redux
+// const render = () => {
+//   ReactDom.render(<FirstRedux />, document.getElementById('root'));
+// };
+// render();
+// store.subscribe(render);
+
+// react-redux
+import { Provider } from 'react-redux';
+// import TwoRedux from './reduxFloder/TwoRedux';
+// import ThreeRedux from './reduxFloder/ThreeRedux';
+// ReactDom.render(
+//   <Provider store={store}>
+//     <ThreeRedux />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+import FourRedux from './reduxFloder/FourRedux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { firstReducer } from './reduxFloder/count.redux';
+const store1 = createStore(firstReducer, applyMiddleware(thunk, logger));
+ReactDom.render(
+  <Provider store={store1}>
+    <FourRedux />
+  </Provider>,
+  document.getElementById('root')
+);
