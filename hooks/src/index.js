aebd58770d5cd0daf2f17776d56823487414d65b
tt <1259747604@qq.com>
2019-12-08 13:06:15 +0800

    提交

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
+import UseContext from './usecontext/UseContext'
 
-ReactDom.render(<App></App>,document.getElementById('root'))
+// ReactDom.render(<App></App>,document.getElementById('root'))
+//使用usecontext
+ReactDom.render(<UseContext></UseContext>,document.getElementById('root'))
--- one/src/index.js
+++ one/src/index.js
@@ -1,13 +1,13 @@
-import React from  'react'
-import ReactDom from  'react-dom'
-// import App from  './App'
-// import LifeCycle from  './LifeCycle'
-// import App1 from  './App1'
+import React from 'react';
+import ReactDom from 'react-dom';
+// import App from './App'
+// import LifeCycle from './LifeCycle'
+// import App1 from './App1';
 // import Purememo from  './Purememo'
 // import Composition from './Composition'
-// import Hoc from './HOC/HOC'
+// import Hoc from './HOC/HOC';
 // import Context1 from './HOC/Context1'
-import Context2 from './HOC/Context2'
+import Context2 from './HOC/Context2';
 
 // ReactDom.render(<App/>,document.getElementById('root'))
 //演示生命周期
@@ -21,4 +21,4 @@ import Context2 from './HOC/Context2'
 //高阶组件
 // ReactDom.render(<Hoc title="一个属性"/>,document.getElementById('root'))
 //组件上下文
-ReactDom.render(<Context2/>,document.getElementById('root'))
+ReactDom.render(<Context2 />, document.getElementById('root'));
